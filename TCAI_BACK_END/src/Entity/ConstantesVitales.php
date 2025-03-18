da39f1ed2ea6007ff469cc25decadf7dc270f276
santiestebanellc <santiestebanellc@gmail.com>
2025-03-18 17:48:03 +0100

    entities

---
+++ TCAI_BACK_END/src/Entity/ConstantesVitales.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConstantesVitalesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConstantesVitalesRepository::class)]
+class ConstantesVitales
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $ta_sistolica = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $ta_diastolica = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $frecuencia_respiratoria = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $pulso = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $temperatura = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $saturacion_oxigeno = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $peso = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $talla = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $diuresis = null;
+
+    #[ORM\Column(length: 45, nullable: true)]
+    private ?string $deposiciones = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 1, nullable: true)]
+    private ?string $stp = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTaSistolica(): ?string
+    {
+        return $this->ta_sistolica;
+    }
+
+    public function setTaSistolica(?string $ta_sistolica): static
+    {
+        $this->ta_sistolica = $ta_sistolica;
+
+        return $this;
+    }
+
+    public function getTaDiastolica(): ?string
+    {
+        return $this->ta_diastolica;
+    }
+
+    public function setTaDiastolica(?string $ta_diastolica): static
+    {
+        $this->ta_diastolica = $ta_diastolica;
+
+        return $this;
+    }
+
+    public function getFrecuenciaRespiratoria(): ?string
+    {
+        return $this->frecuencia_respiratoria;
+    }
+
+    public function setFrecuenciaRespiratoria(?string $frecuencia_respiratoria): static
+    {
+        $this->frecuencia_respiratoria = $frecuencia_respiratoria;
+
+        return $this;
+    }
+
+    public function getPulso(): ?string
+    {
+        return $this->pulso;
+    }
+
+    public function setPulso(?string $pulso): static
+    {
+        $this->pulso = $pulso;
+
+        return $this;
+    }
+
+    public function getTemperatura(): ?string
+    {
+        return $this->temperatura;
+    }
+
+    public function setTemperatura(?string $temperatura): static
+    {
+        $this->temperatura = $temperatura;
+
+        return $this;
+    }
+
+    public function getSaturacionOxigeno(): ?string
+    {
+        return $this->saturacion_oxigeno;
+    }
+
+    public function setSaturacionOxigeno(?string $saturacion_oxigeno): static
+    {
+        $this->saturacion_oxigeno = $saturacion_oxigeno;
+
+        return $this;
+    }
+
+    public function getPeso(): ?string
+    {
+        return $this->peso;
+    }
+
+    public function setPeso(?string $peso): static
+    {
+        $this->peso = $peso;
+
+        return $this;
+    }
+
+    public function getTalla(): ?string
+    {
+        return $this->talla;
+    }
+
+    public function setTalla(?string $talla): static
+    {
+        $this->talla = $talla;
+
+        return $this;
+    }
+
+    public function getDiuresis(): ?string
+    {
+        return $this->diuresis;
+    }
+
+    public function setDiuresis(?string $diuresis): static
+    {
+        $this->diuresis = $diuresis;
+
+        return $this;
+    }
+
+    public function getDeposiciones(): ?string
+    {
+        return $this->deposiciones;
+    }
+
+    public function setDeposiciones(?string $deposiciones): static
+    {
+        $this->deposiciones = $deposiciones;
+
+        return $this;
+    }
+
+    public function getStp(): ?string
+    {
+        return $this->stp;
+    }
+
+    public function setStp(?string $stp): static
+    {
+        $this->stp = $stp;
+
+        return $this;
+    }
+}
---
+++ TCAI_BACK_END/src/Entity/Paciente.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PacienteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PacienteRepository::class)]
+class Paciente
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $num_historial = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $apellidos = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $fecha_nacimiento = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $direccion_completa = null;
+
+    #[ORM\Column(length: 45, nullable: true)]
+    private ?string $lengua_materna = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $antecedentes = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $alergias = null;
+
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $nombre_cuidador = null;
+
+    #[ORM\Column(length: 9, nullable: true)]
+    private ?string $telefono_cuidador = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $fecha_ingreso = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $timestamp = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumHistorial(): ?int
+    {
+        return $this->num_historial;
+    }
+
+    public function setNumHistorial(?int $num_historial): static
+    {
+        $this->num_historial = $num_historial;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(?string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(?string $apellidos): static
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getFechaNacimiento(): ?\DateTimeInterface
+    {
+        return $this->fecha_nacimiento;
+    }
+
+    public function setFechaNacimiento(?\DateTimeInterface $fecha_nacimiento): static
+    {
+        $this->fecha_nacimiento = $fecha_nacimiento;
+
+        return $this;
+    }
+
+    public function getDireccionCompleta(): ?string
+    {
+        return $this->direccion_completa;
+    }
+
+    public function setDireccionCompleta(?string $direccion_completa): static
+    {
+        $this->direccion_completa = $direccion_completa;
+
+        return $this;
+    }
+
+    public function getLenguaMaterna(): ?string
+    {
+        return $this->lengua_materna;
+    }
+
+    public function setLenguaMaterna(?string $lengua_materna): static
+    {
+        $this->lengua_materna = $lengua_materna;
+
+        return $this;
+    }
+
+    public function getAntecedentes(): ?string
+    {
+        return $this->antecedentes;
+    }
+
+    public function setAntecedentes(?string $antecedentes): static
+    {
+        $this->antecedentes = $antecedentes;
+
+        return $this;
+    }
+
+    public function getAlergias(): ?string
+    {
+        return $this->alergias;
+    }
+
+    public function setAlergias(?string $alergias): static
+    {
+        $this->alergias = $alergias;
+
+        return $this;
+    }
+
+    public function getNombreCuidador(): ?string
+    {
+        return $this->nombre_cuidador;
+    }
+
+    public function setNombreCuidador(?string $nombre_cuidador): static
+    {
+        $this->nombre_cuidador = $nombre_cuidador;
+
+        return $this;
+    }
+
+    public function getTelefonoCuidador(): ?int
+    {
+        return $this->telefono_cuidador;
+    }
+
+    public function setTelefonoCuidador(?int $telefono_cuidador): static
+    {
+        $this->telefono_cuidador = $telefono_cuidador;
+
+        return $this;
+    }
+
+    public function getFechaIngreso(): ?\DateTimeInterface
+    {
+        return $this->fecha_ingreso;
+    }
+
+    public function setFechaIngreso(?\DateTimeInterface $fecha_ingreso): static
+    {
+        $this->fecha_ingreso = $fecha_ingreso;
+
+        return $this;
+    }
+
+    public function getTimestamp(): ?\DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(?\DateTimeInterface $timestamp): static
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+}
